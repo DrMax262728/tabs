098caaa6c3d09fb6735e41c6e180c06255048c24
DrMax262728 <perfectworld3843@gmail.com>
2019-04-18 09:37:12 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    let links = document.querySelectorAll('.tablinks li a');
+    let contents = document.querySelectorAll('.tabcontents div');
+
+
+        for (let [iLink, link] of links.entries()) {
+            link.onclick = () => {
+
+                for (let [iContent, content] of contents.entries()) {
+
+                    if (content.classList.contains('show')) {
+                        content.classList.remove('show', 'active');
+                        links[iContent].classList.remove('active');
+                    }
+                }
+
+                contents[iLink].classList.add('show', 'active');
+                link.classList.add('active');
+            }
+        }
+};
